632bafbea5ba64363bc3762ca292734eea2809c4
batorfi <zsolt@batorfi.cloud>
2021-12-03 07:34:38 +0100

    initialized global variables

--- generate_student_codes.py
+++ generate_student_codes.py
@@ -1,8 +1,8 @@
-inport pandas as pd 
+import pandas as pd 
 
-EXPORT_DIR = f'export\'
+EXPORT_DIR = f'export'
 EXPORT_FILENAME = f'EU2K-QR-'
-IMPORT_DIR = f'import\'
+IMPORT_DIR = f'import'
 DATASOURCE_FILE = f'eu2k-users-qrcodes.xslx'
 
 
